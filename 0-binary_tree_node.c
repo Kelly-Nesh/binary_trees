d883e0aa72d0a26d8c5e2c16576c4dc2fef253ad
Kelly-Nesh <cs.kelly.nesh@gmail.com>
2023-02-28 20:27:26 +0300

    reduced code :) understood the question

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -10,59 +10,13 @@
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *newNode;
-	binary_tree_t *swap;
 
 	newNode = malloc(sizeof(binary_tree_t));
 	if (newNode == NULL)
 		return (NULL);
-	if (parent == NULL)
-	{
-		parent = newNode;
-		parent->n = value;
-		return (parent);
-	}
-	else
-	{
-		swap = parent;
-		while (1)
-		{
-			parent = swap;
-			if (value < parent->n)
-			{
-				swap = parent->left;
-				if (swap == NULL)
-				{
-					swap = insert_data(swap, newNode,
-							parent, value, 0);
-					return (swap);
-				}
-			}
-			else
-			{
-				swap = parent->right;
-				if (swap == NULL)
-				{
-					swap = insert_data(swap, newNode,
-							parent, value, 1);
-					return (swap);
-				}
-			}
-		}
-	}
-	return (NULL);
-}
-
-binary_tree_t *insert_data(binary_tree_t *swp, binary_tree_t *new,
-		binary_tree_t *prt, int n, int rl)
-{
-	swp = new;
-	if (rl == 0)
-		prt->left = swp;
-	else
-		prt->right = swp;
-	swp->n = n;
-	swp->parent = prt;
-	swp->left = NULL;
-	swp->right = NULL;
-	return (swp);
+	newNode->n = value;
+	newNode->parent = parent;
+	newNode->right = NULL;
+	newNode->left = NULL;
+	return (newNode);
 }
