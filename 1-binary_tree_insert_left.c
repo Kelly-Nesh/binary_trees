ee5b9015d8598f09e675bf8e6c7a79025cee4324
Kelly-Nesh <cs.kelly.nesh@gmail.com>
2023-03-01 13:37:38 +0300

    added documentation and check for null parent

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,18 +1,30 @@
 #include "binary_trees.h"
 
 /**
- *
+ * binary_tree_insert - inserts aq node as a left child of another node
+ * @parent: a pointer to the node to insert the left child in
+ * @value: the value to store in the new code
+ * Return: NULL if parent is null of in case of failure or pointer to the
+ * new node
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new;
+	binary_tree_t *newNode, *temp;
 
-	new = malloc(sizeof(binary_tree_t));
-	if (!new)
+	newNode = malloc(sizeof(binary_tree_t));
+	if (newNode == NULL || parent == NULL)
 		return (NULL);
-	parent->left = new;
-	new->parent = parent;
-	new->right = NULL;
-	new->left = NULL;
-	new->n = value;
+	if (parent->left)
+	{
+		temp = parent->left;
+		temp->parent = newNode;
+		newNode->left = temp;
+	}
+	else
+		newNode->left = NULL;
+	parent->left = newNode;
+	newNode->n = value;
+	newNode->parent = parent;
+	newNode->right = NULL;
+	return (newNode);
 }
