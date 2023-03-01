1bf27e79bb2d3c73b659f2504797173812a1c487
Kelly-Nesh <cs.kelly.nesh@gmail.com>
2023-03-01 13:54:51 +0300

    returned else

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -20,6 +20,8 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 		temp->parent = newNode;
 		newNode->left = temp;
 	}
+	else
+		newNode->left = NULL;
 	parent->left = newNode;
 	newNode->n = value;
 	newNode->parent = parent;
