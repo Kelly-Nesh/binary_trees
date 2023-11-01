a5302026d1fbb538132c636a8222217a2861d897
Kelly-Nesh <cs.kelly.nesh@gmail.com>
2023-11-01 17:19:55 +0300

    betty check pass

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -2,8 +2,9 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_depth - measures the height of a binary tree
- * @tree - pointer to the node to measure the depth
+ * binary_tree_depth - measures the depth of a node in a binary tree
+ * @tree: pointer to the node to measure the depth
+ * Return: depth of binary tree or 0 if tree is NULL
  */
 
 size_t binary_tree_depth(const binary_tree_t *tree)
